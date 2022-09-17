7612db255ef2390983ce3b9bac2224c7595fc8db
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-09-17 20:34:07 +0430

    refactor: unit tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,17 @@
-import React, { StrictMode } from 'react';
-import { BlockNumberProvider } from 'lib/hooks/useBlockNumber';
-import { MulticallUpdater } from 'lib/state/multicall';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
-import Web3Provider from './components/Web3Provider';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import store from './state';
-import ApplicationUpdater from './state/application/updater';
-import TransactionUpdater from './state/transactions/updater';
-import UserUpdater from './state/user/updater';
 
 import './index.sass';
+import Main from './Main';
 
 if (!!window.ethereum) {
   window.ethereum.autoRefreshOnNetworkChange = false;
 }
 
-function Updaters() {
-  return (
-    <>
-      <UserUpdater />
-      <ApplicationUpdater />
-      <TransactionUpdater />
-      <MulticallUpdater />
-    </>
-  );
-}
-
 const container = document.getElementById('root') as HTMLElement;
 
-createRoot(container).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Web3Provider>
-          <BlockNumberProvider>
-            <Updaters />
-            <App />
-          </BlockNumberProvider>
-        </Web3Provider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-);
+createRoot(container).render(Main());
 
 if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
   serviceWorkerRegistration.register();
