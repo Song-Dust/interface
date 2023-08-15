d346261da0ae5bde3cfde1d9fb47938eccfd250b
VALKONX13 <valkonx13@gmail.com>
2023-08-15 22:39:36 +0330

    new song-a-day song modal details

--- src/components/modal/AddSongModal.tsx
+++ src/components/modal/AddSongModal.tsx
@@ -171,16 +171,28 @@ const AddSongModal = (props: ModalPropsInterface) => {
 
   return (
     <Modal
-      className={'!max-w-4xl w-full overflow-visible right-full'}
+      className={'!max-w-4xl w-full overflow-visible right-full px-10'}
       title={`Select the song you want to add to this category`}
       closeModal={props.closeModal}
       open={props.open}
     >
       <InstantSearch searchClient={searchClient} indexName="songs" routing>
-        <div style={{ width: '100%' }}>
-          <SearchBox placeholder="Find songs by name, location, instrument and more" />
-          <Stats />
+        <div className='w-full flex items-baseline gap-5'>
+          <SearchBox placeholder='Find songs by name, location, instrument and more' classNames={{
+            root: 'grow',
+            form: 'relative',
+            input: 'block w-full pl-7 px-4 py-2 border border-slate-400 placeholder-slate-600 focus:outline-none focus:border-pink-500 focus:ring-pink-500 rounded-xl focus:ring-1',
+            submitIcon: 'absolute top-4 left-0 bottom-0 w-8',
+          }} />
+          <div className='flex items-center gap-2'>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9 2C9 1.46875 9.4375 1 10 1H14C14.5312 1 15 1.46875 15 2V6C15 6.5625 14.5312 7 14 7C13.4375 7 13 6.5625 13 6V4.4375L7.6875 9.71875C7.3125 10.125 6.65625 10.125 6.28125 9.71875C5.875 9.34375 5.875 8.6875 6.28125 8.3125L11.5625 3H10C9.4375 3 9 2.5625 9 2Z" fill="#193154" />
+              <path opacity="0.4" d="M1 4C1 2.90625 1.875 2 3 2H6C6.53125 2 7 2.46875 7 3C7 3.5625 6.53125 4 6 4H3V13H12V10C12 9.46875 12.4375 9 13 9C13.5312 9 14 9.46875 14 10V13C14 14.125 13.0938 15 12 15H3C1.875 15 1 14.125 1 13V4Z" fill="#193154" />
+            </svg>
+            <p className='text-sm font-normal text-black'>Click here for advanced exploration</p>
+          </div>
         </div>
+        <Stats />
         <CustomHits />
       </InstantSearch>
       <Transition
--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -102,8 +102,20 @@ const Category = () => {
         open={moreActionModalOpen}
       >
         <main className={'flex flex-wrap gap-6 flex-col items-start mt-4'}>
-          <button className='bg-neutral-200 rounded-xl w-full py-3 text-start text-lg font-semibold text-black'>new category</button>
-          <button className='bg-neutral-200 rounded-xl w-full py-3 text-start text-lg font-semibold text-black' onClick={openAddSongModal} onClickCapture={closeMoreActionModal} >new Song-a-day song</button>
+          <div className='bg-neutral-200 rounded-xl flex w-full items-center px-3 gap-2'>
+            <svg width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M25.5 17.0695C25.5 20.1945 22.9609 22.6945 19.875 22.6945C16.75 22.6945 14.25 20.1945 14.25 17.0695C14.25 13.9835 16.75 11.4445 19.875 11.4445C22.9609 11.4445 25.5 13.9835 25.5 17.0695ZM19.25 14.5695V16.4445H17.375C17.0234 16.4445 16.75 16.757 16.75 17.0695C16.75 17.421 17.0234 17.6945 17.375 17.6945H19.25V19.5695C19.25 19.921 19.5234 20.1945 19.875 20.1945C20.1875 20.1945 20.5 19.921 20.5 19.5695V17.6945H22.375C22.6875 17.6945 23 17.421 23 17.0695C23 16.757 22.6875 16.4445 22.375 16.4445H20.5V14.5695C20.5 14.257 20.1875 13.9445 19.875 13.9445C19.5234 13.9445 19.25 14.257 19.25 14.5695Z" fill="#353535" />
+              <path opacity="0.4" d="M3 22.6945C1.59375 22.6945 0.5 21.6007 0.5 20.1945V11.4445C0.5 10.0773 1.59375 8.94446 3 8.94446H18C18.8984 8.94446 19.7188 9.45227 20.1484 10.2335C20.0703 10.2335 19.9531 10.1945 19.875 10.1945C16.0469 10.1945 13 13.2804 13 17.0695C13 19.4132 14.1328 21.4835 15.8906 22.6945H3ZM17.6875 7.69446H3.3125C2.76562 7.69446 2.375 7.30383 2.375 6.75696C2.375 6.24915 2.76562 5.81946 3.3125 5.81946H17.6875C18.1953 5.81946 18.625 6.24915 18.625 6.75696C18.625 7.30383 18.1953 7.69446 17.6875 7.69446ZM15.8125 4.56946H5.1875C4.64062 4.56946 4.25 4.17883 4.25 3.63196C4.25 3.12415 4.64062 2.69446 5.1875 2.69446H15.8125C16.3203 2.69446 16.75 3.12415 16.75 3.63196C16.75 4.17883 16.3203 4.56946 15.8125 4.56946Z" fill="#EC2A64" />
+            </svg>
+            <button className='py-3 text-start text-base font-semibold text-black'>new category</button>
+          </div>
+          <div className='bg-neutral-200 rounded-xl flex w-full items-center px-3 gap-2'>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M23.25 16.375C23.25 19.5 20.7109 22 17.625 22C14.5 22 12 19.5 12 16.375C12 13.2891 14.5 10.75 17.625 10.75C20.7109 10.75 23.25 13.2891 23.25 16.375ZM17 13.875V15.75H15.125C14.7734 15.75 14.5 16.0625 14.5 16.375C14.5 16.7266 14.7734 17 15.125 17H17V18.875C17 19.2266 17.2734 19.5 17.625 19.5C17.9375 19.5 18.25 19.2266 18.25 18.875V17H20.125C20.4375 17 20.75 16.7266 20.75 16.375C20.75 16.0625 20.4375 15.75 20.125 15.75H18.25V13.875C18.25 13.5625 17.9375 13.25 17.625 13.25C17.2734 13.25 17 13.5625 17 13.875Z" fill="#353535" />
+              <path opacity="0.4" d="M15.75 3.25C17.1172 3.25 18.25 4.38281 18.25 5.75V9.53906C18.0156 9.53906 17.8203 9.5 17.625 9.5C16.8047 9.5 16.0625 9.65625 15.3594 9.89062C14.5 7.46875 12.1953 5.75 9.46094 5.75C6.02344 5.75 3.21094 8.5625 3.21094 12C3.21094 15.4766 6.02344 18.25 9.46094 18.25C9.96875 18.25 10.4766 18.2109 10.9453 18.0938C11.1797 19.1094 11.6484 20.0078 12.3125 20.75H3.25C1.84375 20.75 0.75 19.6562 0.75 18.25V5.75C0.75 4.38281 1.84375 3.25 3.25 3.25H15.75ZM10.7109 12C10.7109 12.7031 10.1641 13.25 9.46094 13.25C8.79688 13.25 8.21094 12.7031 8.21094 12C8.21094 11.3359 8.79688 10.75 9.46094 10.75C10.1641 10.75 10.7109 11.3359 10.7109 12Z" fill="#EC2A64" />
+            </svg>
+            <button className='py-3 text-start text-base font-semibold text-black' onClick={openAddSongModal} onClickCapture={closeMoreActionModal} >new Song-a-day song</button>
+          </div>
         </main>
       </Modal>
 
@@ -128,7 +140,7 @@ const Category = () => {
                 // faMagnifyingGlass
               }
               placeholder={'Search songs in this category'}
-              onUserInput={() => {}}
+              onUserInput={() => { }}
             ></Input>
             <Input
               type={'submit'}
@@ -139,7 +151,7 @@ const Category = () => {
               }
               toggle
               toggleLabel={'Default View'}
-              onUserInput={() => {}}
+              onUserInput={() => { }}
             ></Input>
             {/*<Input type="button"></Input>*/}
           </header>
